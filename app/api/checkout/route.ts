d11cdb203085388f45a522b37e3cca3c2b46222f
Federico Triebel <ftriebel@icloud.com>
2026-06-09 14:44:49 -0700

    Remove Stripe temporarily

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,30 +1,5 @@
-import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-03-25.dahlia",
-});
-
-export async function POST(req: Request) {
-  const { amount } = await req.json();
-
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    mode: "payment",
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "Luxury Travel Package",
-          },
-          unit_amount: amount * 100,
-        },
-        quantity: 1,
-      },
-    ],
-    success_url: "http://localhost:3000",
-    cancel_url: "http://localhost:3000",
-  });
-
-  return Response.json({ url: session.url });
+export async function POST() {
+  return Response.json({
+    success: true
+  })
 }
