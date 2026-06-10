4aa3b4983a8505c5e374288025ecb06f2f14c278
Federico Triebel <ftriebel@icloud.com>
2026-06-10 00:18:19 -0700

    Fix ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,5 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from "eslint-config-next/core-web-vitals.js";
 
-const eslintConfig = defineConfig([
+export default [
   ...nextVitals,
-  ...nextTs,
-  {
-    rules: {
-      "@next/next/no-img-element": "off",
-    },
-  },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+];
